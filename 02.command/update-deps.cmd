3f5611dc5cf5e5666d9ae37ffec91c725432b511
Jaime <jpstill85@gmail.com>
2020-03-19 16:16:44 -0400

    update deps

---
+++ 02.command/update-deps.cmd
@@ -0,0 +1,3 @@
+call yarn add @angular/animations @angular/cdk @angular/common @angular/compiler @angular/core @angular/flex-layout @angular/forms @angular/material @angular/platform-browser @angular/platform-browser-dynamic @angular/router hammerjs rxjs tslib zone.js
+
+call yarn add -D @angular-devkit/build-angular @angular/cli @angular/compiler-cli @angular/language-service @fortawesome/fontawesome-free @types/node material-icons roboto-fontface ts-node tslint typescript
